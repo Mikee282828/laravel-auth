96303bb5f6b12cd8c83a4ee3759af9228bb21ad8
michele laptop <micheledong2803@gmail.com>
2024-07-09 16:05:30 +0200

    Setup seeder, resource controller, source route

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\DashboardController; //<---- Import del controller precedentemente creato!
+use App\Http\Controllers\ProjectController;
 
 /*
 |--------------------------------------------------------------------------
@@ -28,5 +29,7 @@ Route::middleware(['auth'])
         // - il nome della rotta ->name("dashboard") diventa ->name("admin.dashboard")
         Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
     });
+
+Route::resource('/', ProjectController::class);
 
 require __DIR__ . '/auth.php';
