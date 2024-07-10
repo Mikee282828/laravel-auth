33bef9bdcdad8dd997374d752c73bdc309364227
MikeMac <micheledong2803@gmail.com>
2024-07-10 16:52:34 +0200

    functional edit update

--- resources/views/admin/projects/edit.blade.php
+++ resources/views/admin/projects/edit.blade.php
@@ -1,48 +1,51 @@
 @extends('layouts.app')
 
 @section('content')
-	<h1 class="bg-warning p-2 m-0">Questo è l'edit</h1>
-	<div class="container">
+    <h1 class="bg-warning p-2 m-0">Questo è l'edit</h1>
+    <div class="container">
 
-		{{-- MESSAGGIO DI ERRORE SE NON SI COMPLETANO I CAMPI CHE SONO OBBLIGATORI --}}
-		<div>
-			@if ($errors->any())
-				<div class="alert alert-danger">
-					<ul>
-						@foreach ($errors->all() as $error)
-							<li>{{ $error }}</li>
-						@endforeach
-					</ul>
-				</div>
-			@endif
-		</div>
+        {{-- MESSAGGIO DI ERRORE SE NON SI COMPLETANO I CAMPI CHE SONO OBBLIGATORI --}}
+        <div>
+            @if ($errors->any())
+                <div class="alert alert-danger">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            @endif
+        </div>
 
-		<form method="POST" action="{{ route('admin.projects.update') }}">
-			@method('PUT')
-			@csrf
-			<div class="mb-3">
-				<label for="exampleInputEmail1" class="form-label">Email address</label>
-				<input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-				<div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
-				@error('email')
-					<div>{{ $message }}</div>
-				@enderror
-			</div>
-			<div class="mb-3">
-				<label for="exampleInputPassword1" class="form-label">Password</label>
-				<input type="password" class="form-control" id="exampleInputPassword1">
-				@error('password')
-					<div>{{ $message }}</div>
-				@enderror
-			</div>
-			<div class="mb-3 form-check">
-				<input type="checkbox" class="form-check-input" id="exampleCheck1">
-				<label class="form-check-label" for="exampleCheck1">Check me out</label>
-				@error('password')
-					<div>{{ $message }}</div>
-				@enderror
-			</div>
+        <form method="POST" action="{{ route('admin.projects.update',$project->id) }}">
+            @method('PUT')
+
+            @csrf
+            <div class="mb-3">
+                <label for="title" class="form-label">TITLE</label>
+                <input type="text" class="form-control" name="title" value="{{ old('title',$project->title) }}">
+                @error('title')
+                    <div class="form-text text-danger">{{ $message }}</div>
+                @enderror
+            </div>
+
+            <div class="mb-3">
+                <label for="description" class="form-label">DESCRIPTION</label>
+                <input type="text" class="form-control" name="description" value="{{ old('description',$project->description) }}">
+                @error('description')
+                    <div class="form-text text-danger">{{ $message }}</div>
+                @enderror
+            </div>
+
+            <div class="mb-3">
+                <label for="img_preview" class="form-label">Link Preview</label>
+                <input type="text" class="form-control" name="img_preview" value="{{ old('title',$project->img_preview) }}">
+                @error('img_preview')
+                    <div class="form-text text-danger">The Link Preview field is required.</div>
+                @enderror
+            </div>
 			<button type="submit" class="btn btn-primary">Submit</button>
-		</form>
-	</div>
+
+        </form>
+    </div>
 @endsection
