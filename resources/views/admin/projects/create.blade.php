1c845a989a0e0b4b20230d2ce8a069b270e66552
GiuseppeDeRosa1992 <derosagiuseppe1092@gmail.com>
2024-07-10 18:15:47 +0200

    migliorative layout

--- resources/views/admin/projects/create.blade.php
+++ resources/views/admin/projects/create.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 	<div class="container">
-		<h1>Questo è il Create</h1>
+		<h1 class="text-center py-2">Crea Nuovo Progetto</h1>
 
 		{{-- MESSAGGIO DI ERRORE SE NON SI COMPLETANO I CAMPI CHE SONO OBBLIGATORI --}}
 		<div>
@@ -22,30 +22,30 @@
 			@csrf
 
 			<div class="mb-3">
-				<label for="title" class="form-label">TITLE</label>
-				<input type="text" class="form-control" name="title" value="{{old('title')}}">
+				<label for="title" class="form-label">Titolo Nuovo Progetto</label>
+				<input type="text" class="form-control" name="title" value="{{ old('title') }}">
 				@error('title')
 					<div class="form-text text-danger">{{ $message }}</div>
 				@enderror
 			</div>
 
 			<div class="mb-3">
-				<label for="description" class="form-label">DESCRIPTION</label>
-				<input type="text" class="form-control" name="description" value="{{old('description')}}">
+				<label for="description" class="form-label">Descrizione Nuovo Progetto</label>
+				<input type="text" class="form-control" name="description" value="{{ old('description') }}">
 				@error('description')
 					<div class="form-text text-danger">{{ $message }}</div>
 				@enderror
 			</div>
 
 			<div class="mb-3">
-				<label for="img_preview" class="form-label">Link Preview</label>
-				<input type="text" class="form-control" name="img_preview" value="{{old('title')}}">
+				<label for="img_preview" class="form-label">URL Immagine Nuovo Progetto</label>
+				<input type="text" class="form-control" name="img_preview" value="{{ old('title') }}">
 				@error('img_preview')
 					<div class="form-text text-danger">The Link Preview field is required.</div>
 				@enderror
 			</div>
 
-			<button type="submit" class="btn btn-primary">Submit</button>
+			<button type="submit" class="btn btn-outline-primary">Invia Nuovo Progetto</button>
 		</form>
 	</div>
 @endsection
--- resources/views/admin/projects/edit.blade.php
+++ resources/views/admin/projects/edit.blade.php
@@ -1,51 +1,52 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1 class="bg-warning p-2 m-0">Questo è l'edit</h1>
-    <div class="container">
+	<h1 class="py-2 m-0 text-danger text-center">Modifica Progetto</h1>
+	<div class="container">
 
-        {{-- MESSAGGIO DI ERRORE SE NON SI COMPLETANO I CAMPI CHE SONO OBBLIGATORI --}}
-        <div>
-            @if ($errors->any())
-                <div class="alert alert-danger">
-                    <ul>
-                        @foreach ($errors->all() as $error)
-                            <li>{{ $error }}</li>
-                        @endforeach
-                    </ul>
-                </div>
-            @endif
-        </div>
+		{{-- MESSAGGIO DI ERRORE SE NON SI COMPLETANO I CAMPI CHE SONO OBBLIGATORI --}}
+		<div>
+			@if ($errors->any())
+				<div class="alert alert-danger">
+					<ul>
+						@foreach ($errors->all() as $error)
+							<li>{{ $error }}</li>
+						@endforeach
+					</ul>
+				</div>
+			@endif
+		</div>
 
-        <form method="POST" action="{{ route('admin.projects.update',$project->id) }}">
-            @method('PUT')
+		<form method="POST" action="{{ route('admin.projects.update', $project->id) }}">
+			@method('PUT')
 
-            @csrf
-            <div class="mb-3">
-                <label for="title" class="form-label">TITLE</label>
-                <input type="text" class="form-control" name="title" value="{{ old('title',$project->title) }}">
-                @error('title')
-                    <div class="form-text text-danger">{{ $message }}</div>
-                @enderror
-            </div>
+			@csrf
+			<div class="mb-3">
+				<label for="title" class="form-label">Modifica Titolo Progetto</label>
+				<input type="text" class="form-control" name="title" value="{{ old('title', $project->title) }}">
+				@error('title')
+					<div class="form-text text-danger">{{ $message }}</div>
+				@enderror
+			</div>
 
-            <div class="mb-3">
-                <label for="description" class="form-label">DESCRIPTION</label>
-                <input type="text" class="form-control" name="description" value="{{ old('description',$project->description) }}">
-                @error('description')
-                    <div class="form-text text-danger">{{ $message }}</div>
-                @enderror
-            </div>
+			<div class="mb-3">
+				<label for="description" class="form-label">Modifica Descrizione Progetto</label>
+				<input type="text" class="form-control" name="description"
+					value="{{ old('description', $project->description) }}">
+				@error('description')
+					<div class="form-text text-danger">{{ $message }}</div>
+				@enderror
+			</div>
 
-            <div class="mb-3">
-                <label for="img_preview" class="form-label">Link Preview</label>
-                <input type="text" class="form-control" name="img_preview" value="{{ old('title',$project->img_preview) }}">
-                @error('img_preview')
-                    <div class="form-text text-danger">The Link Preview field is required.</div>
-                @enderror
-            </div>
-			<button type="submit" class="btn btn-primary">Submit</button>
+			<div class="mb-3">
+				<label for="img_preview" class="form-label">Modifica URL IMmagine Progetto</label>
+				<input type="text" class="form-control" name="img_preview" value="{{ old('title', $project->img_preview) }}">
+				@error('img_preview')
+					<div class="form-text text-danger">The Link Preview field is required.</div>
+				@enderror
+			</div>
+			<button type="submit" class="btn btn-outline-danger">Modifica Progetto</button>
 
-        </form>
-    </div>
+		</form>
+	</div>
 @endsection
