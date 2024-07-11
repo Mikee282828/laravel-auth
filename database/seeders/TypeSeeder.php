70d09b0fb6d60be4f7b8d312be181e6068ba7f3f
MikeMac <micheledong2803@gmail.com>
2024-07-11 16:01:56 +0200

    Type - seeder

--- database/seeders/TypeSeeder.php
+++ database/seeders/TypeSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Type;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,28 @@ class TypeSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $newType = new Type();
+        $newType->name = 'Front-end';
+        $newType->description = 'Wrote mainly in html css jss';
+        $newType->icon = '<i class="fa-solid fa-window-maximize"></i>';
+        $newType->save();
+
+        $newType = new Type();
+        $newType->name = 'Back-end';
+        $newType->description = 'Wrote mainly in php and blade';
+        $newType->icon = '<i class="fa-solid fa-server"></i>';
+        $newType->save();
+
+        $newType = new Type();
+        $newType->name = 'Full stack';
+        $newType->description = 'Wrote with front and back end languages';
+        $newType->icon = '<i class="fa-solid fa-code"></i>';
+        $newType->save();
+
+        $newType = new Type();
+        $newType->name = 'Design';
+        $newType->description = "it's stylish!";
+        $newType->icon = '<i class="fa-solid fa-marker"></i>';
+        $newType->save();
     }
 }
